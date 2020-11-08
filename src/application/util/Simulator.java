a0bc7878552c119a0b25d41f22d0f42362a0c5c6
Max Drexler <mndrexler@gmail.com>
2020-11-07 22:41:24 -0600

    finished simulation class commenting

--- src/application/util/Simulator.java
+++ src/application/util/Simulator.java
@@ -13,20 +13,31 @@ import javafx.scene.shape.StrokeLineJoin;
 import javafx.util.Pair;
 
 /**
- * @author Max Drexler
+ * Simulator class that contains all important methods for the function of
+ * conway's game of life simulation
  *
+ * <pre>
+ *  Rules of Conway's Game of Life:
+ * 	1. Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+ * 	2. Any live cell with two or three live neighbors lives on to the next generation.
+ * 	3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+ * 	4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
+ * </pre>
+ * 
+ * @author Max Drexler
  */
 public class Simulator extends Canvas {
 	private Thread drawThread;
 	private SimulationThread runnable;
 	private GraphicsContext gc;
-	private Rectangle viewBox;
-	private boolean[][] board;
+	private boolean[][] board; // data structure used to represent living and dead cells in a grid
+	private Rectangle viewBox; // a resizable area of the board array that is displayed on the screen
 	private int generation;
 
 	private static final int BOARD_WIDTH = 500;
 	private static final int BOARD_HEIGHT = 250;
-	private static final int BOARD_BORDER = 5;
+	private static final int BOARD_BORDER = 5; // viewbox never expands past the boarder giving the illusion of an
+												// infinite plane
 
 	public Simulator(double width, double height) {
 		super(width, height);
@@ -34,7 +45,7 @@ public class Simulator extends Canvas {
 		this.drawThread = new Thread(runnable = new SimulationThread(this));
 		this.gc = this.getGraphicsContext2D();
 		this.generation = 0;
-		this.viewBox = new Rectangle(241, 118, 9, 4);
+		this.viewBox = new Rectangle(241, 118, 9, 4); // default area of board array to be displayed
 
 		gc.setLineWidth(1.0);
 		gc.setLineCap(StrokeLineCap.SQUARE);
@@ -42,6 +53,9 @@ public class Simulator extends Canvas {
 		gc.setMiterLimit(10);
 		gc.setLineJoin(StrokeLineJoin.ROUND);
 
+		/**
+		 * toggles in board array and draws to screen the cell where mouse is clicked
+		 */
 		this.setOnMouseClicked((MouseEvent e) -> {
 			int screenToBoardX = (int) (e.getX() * this.viewBox.getWidth() / (double) this.getWidth());
 			int screenToBoardY = (int) (e.getY() * this.viewBox.getHeight() / (double) this.getHeight());
@@ -119,14 +133,16 @@ public class Simulator extends Canvas {
 			Main.setGen(this.generation); // updates generation label
 		});
 		this.generation++;
-		ArrayList<Pair<Integer, Integer>> list = new ArrayList<>();
+		ArrayList<Pair<Integer, Integer>> list = new ArrayList<>(); // cells to update this generation, used to update
+																	// all cells at same time
 		for (int x = 0; x < board.length; x++) {
 			for (int y = 0; y < board[0].length; y++) {
-				if (board[x][y]) {
-					if (calculateLiveNeighbors(x, y) > 3 || calculateLiveNeighbors(x, y) < 2)
+				if (board[x][y]) { // if cell "living"
+					if (calculateLiveNeighbors(x, y) > 3 || calculateLiveNeighbors(x, y) < 2) // Rule 1 & 3
 						list.add(new Pair<Integer, Integer>(x, y));
+					// Rule 2, living cells that don't die aren't updated
 				} else {
-					if (calculateLiveNeighbors(x, y) == 3)
+					if (calculateLiveNeighbors(x, y) == 3) // Rule 4
 						list.add(new Pair<Integer, Integer>(x, y));
 				}
 			}
@@ -138,7 +154,6 @@ public class Simulator extends Canvas {
 		// viewbox)
 		list.stream().filter(x -> this.viewBox.inBounds(x.getKey(), x.getValue()))
 				.forEach(e -> drawNeighbors(e.getKey(), e.getValue()));
-
 	}
 
 	/*-
@@ -146,8 +161,8 @@ public class Simulator extends Canvas {
 	 * are drawn to the screen
 	 * 
 	 * @param factor in which to grow or shrink the screen
-	 *-		factor < 1 : increase viewbox size -> increase amount of cells drawn to screen
-	 *- 	factor > 1 : decrease viewbox size -> decrease amount of cells drawn to screen
+	 *		factor < 1 : increase viewbox size -> increase amount of cells drawn to screen
+	 *	    factor > 1 : decrease viewbox size -> decrease amount of cells drawn to screen
 	 */
 	public void resizeScreen(double factor) {
 		int newWidth = (int) ((this.board.length - 2 * BOARD_BORDER) / factor);
@@ -226,7 +241,6 @@ public class Simulator extends Canvas {
 				gc.strokeRect(x * this.getWidth() / this.viewBox.getWidth(),
 						y * this.getHeight() / this.viewBox.getHeight(), this.getWidth() / this.viewBox.getWidth(),
 						this.getHeight() / this.viewBox.getHeight());
-
 			}
 		}
 	}
