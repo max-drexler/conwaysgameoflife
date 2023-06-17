1da6b8300e04f46446154dd37f74337ab2399465
Max Drexler <mndrexler@gmail.com>
2023-06-16 20:36:44 -0500

    added maven dependencies to build jar

---
+++ src/main/java/drexler/max/gameoflife/GUIStarter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drexler.max.gameoflife;
+
+import drexler.max.gameoflife.ui.Main;
+/**
+ *
+ * @author mdrex
+ */
+public class GUIStarter {
+    
+    public static void main(final String[] args){
+        Main.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
 module drexler.max.gameoflife {
     requires javafx.controls;
-    exports drexler.max.gameoflife.util;
+    exports drexler.max.gameoflife;
 }
